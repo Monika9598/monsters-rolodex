b14480e36ff979ca25f38002139b2feb0a6c0f9e
Monika9598 <singhgurjarmonika@gmail.com>
2020-03-17 23:19:36 +0530

    Updates

---
+++ precache-manifest.3829d379951415adf432a070ced6be63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eaf38a0bbe43db5443409e5599f12e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eaedaf693392386004a4",
+    "url": "/monsters-rolodex/static/css/main.954838ef.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaedaf693392386004a4",
+    "url": "/monsters-rolodex/static/js/main.7634e1a1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
